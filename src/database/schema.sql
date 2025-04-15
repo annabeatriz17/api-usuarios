650b189285b9f01b5e582cdf2be1d0df360ffba7
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-15 15:24:05 -0300

    first commit

---
+++ src/database/schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE users;
+
+\c users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE post (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+)
+
+INSERT INTO usuarios (name, email, password) VALUES 
+('Valentim', 'valentim@gmail.com', 'ds564');
+('BeatrizLima', 'bealima@gmail.com', 'ds564');
+('Luana', 'luana@gmail.com', 'ds564');
+('Parma', 'parma@gmail.com', 'ds564');
+('Leme', 'leme@gmail.com', 'ds564');
+
+INSERT INTO post (title, content, user_id) VALUES
+    ('Post 1', 'Conteúdo do post 1', 1),
+    ('Post 2', 'Conteúdo do post 2', 2),
+    ('Post 3', 'Conteúdo do post 3', 
