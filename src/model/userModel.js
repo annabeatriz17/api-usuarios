78942dfb6b6fc58c9ea5c090704af1d212428ba7
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-15 15:44:37 -0300

    Create userModel.js

---
+++ src/model/userModel.js
@@ -0,0 +1,32 @@
+const pool = require("../config/database");
+
+const getUsers = async (name) => {
+    const result = await pool.query("SELECT * FROM usuarios");
+    return result.rows;
+};
+
+const getUserById = async (id) => {
+    const result = await pool.query("SELECT * FROM usuarios WHERE id = $1", [id]);
+    return result.rows[0];
+};
+
+const updatePost = async (id, name, email, password) => {
+    const { name, email, password } = data;
+    const result = await pool.query("UPDATE usuarios SET name = $1, email = $2, password = $3 WHERE id = $4 RETURNING *", [name, email, password, id]);
+    return result.rows[0];
+};
+
+const createUser = async (name, email, password) => {
+    const result = await pool.query(
+        "INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+        [name, email, password]
+    );
+    return result.rows[0];
+};
+
+const deleteUser = async (id) => {
+    const result = await pool.query("DELETE FROM usuarios WHERE id = $1", [id]);
+    return result.rows;
+};
+
+module.exports = { getUsers, getUserById, updateUser, createUser, deleteUser };
